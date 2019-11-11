e19e0f3b8d941f47abb1a1c5196b1e179997152e
Ikechukwu Okalia <me@sirlouis.io>
2019-11-11 18:05:59 +0100

    Package name changed

--- app.init.php
+++ app.init.php
@@ -1,10 +1,10 @@
 <?php
 // load up app settings
 $conf_path = \str_replace([
-  "/vendor/7os/web-app-support",
-  "/dev/7os/web-app-support",
-  "\\vendor\\7os\\web-app-support",
-  "\\dev\\7os\\web-app-support",
+  "/vendor/7os/support-soswapp",
+  "/dev/7os/support-soswapp",
+  "\\vendor\\7os\\support-soswapp",
+  "\\dev\\7os\\support-soswapp",
 ],"",__DIR__);
 $conf_file = $conf_path . "/project-info.json";
 
