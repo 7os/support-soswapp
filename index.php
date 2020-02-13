3fc2ed78e9d4a7a1786ed3c1143508ea463041ae
Sir Louis <me@sirlouis.io>
2020-02-13 02:15:34 +0100

    bug fix

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ namespace TymFrontiers;
 use \Michelf\Markdown;
 require_once "app.init.php";
 require_once APP_BASE_INC;
+$page_name = "support";
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr" manifest="<?php echo WHOST; ?>/site.webmanifest">
